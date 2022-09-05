7e8b790c0807ed0a9aac4f036421404105c56202
Krishna Venkatramani (Rain Work GPGPassphrase) <krishna@rain.bh>
2022-09-05 22:00:11 +0400

    Initial Commit

---
+++ StyleUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StyleUI
+//
+//  Created by Krishna Venkatramani on 05/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StyleUI/StyleUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleUIApp.swift
+//  StyleUI
+//
+//  Created by Krishna Venkatramani on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct StyleUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
